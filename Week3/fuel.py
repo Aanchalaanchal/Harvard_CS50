55ffbeefd5ba1caf42be4efc4515da6c272ca83a
Aanchalaanchal <79412247+Aanchalaanchal@users.noreply.github.com>
2022-09-13 18:27:46 -0500

    Update fuel.py

--- Week3/fuel.py
+++ Week3/fuel.py
@@ -1,6 +1,5 @@
 def main():
-    x, y = get_frac()
-    perc = int(x)*100/int(y)
+    perc = get_frac()
     if perc <= 1:
         print("E")
     elif perc >= 99:
@@ -12,8 +11,10 @@ def get_frac():
     while True:
         try:
             num, den = input("Give me a fraction as x/y: ").split('/')
-            if int(num) < int(den):
-                return int(num), int(den)
+            new_num = int(num)
+            new_den = int(den)
+            if new_num < new_den:
+                return new_num*100/new_den
         except (ValueError, ZeroDivisionError):
             pass
         
